e5cc429aa1980dd3d865ca4ef2eb1c6ee8821f29
Mayank <813mayank@gmail.com>
2021-07-01 19:58:03 +0530

    first commit

---
+++ src/main/java/com/example/reactivestream/ReactiveSteamApplication.java
@@ -0,0 +1,17 @@
+package com.example.reactivestream;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSteamApplication {
+
+	 
+	public static void main(String[] args) {
+	System.out.println("hello");
+		SpringApplication.run(ReactiveSteamApplication.class, args);
+	}
+
+
+}
